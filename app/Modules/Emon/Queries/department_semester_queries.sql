1000ebc99c2bf46748437b8e5c0acce5a05189a1
nahidemon <nahidhasanemon21@gmail.com>
2026-05-20 02:59:49 +0600

    Update department_semester_queries.sql

--- app/Modules/Emon/Queries/department_semester_queries.sql
+++ app/Modules/Emon/Queries/department_semester_queries.sql
@@ -1,3 +1,5 @@
+--Show every department and count how many students belong to each department.
+
 SELECT d.department_name, COUNT(s.id) AS student_count
 FROM departments d
 LEFT JOIN students s ON s.department_id = d.id
